c3de26d4cc3ec13146740c008da5af684506682d
EriBjorn <ebjornoy@gmail.com>
2019-10-08 15:41:37 +0200

    Implemented waitServerRespons()

--- ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
+++ ChatClient/src/main/java/no/ntnu/datakomm/chat/TCPClient.java
@@ -187,11 +187,24 @@ public class TCPClient {
     private String waitServerResponse()
     {
 
-        // TODO Step 3: Implement this method
-        // TODO Step 4: If you get I/O Exception or null from the stream, it means that something has gone wrong
-        // with the stream and hence the socket. Probably a good idea to close the socket in that case.
+        String response = null;
 
-        return null;
+
+        try
+        {
+            response = fromServer.readLine();
+
+        }
+        catch (IOException e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+
+        // with the stream and hence the socket. Probably a good idea to close the socket in that case.
+        return response;
+
+
     }
 
 
